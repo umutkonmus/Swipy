3066266871272801dcf4144b95412cae675839d7
Umut Konmus <umutkonmus@gmail.com>
2025-03-22 14:41:05 +0300

    Initial Commit

---
+++ SwipyTests/SwipyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipyTests.swift
+//  SwipyTests
+//
+//  Created by Umut Konmuş on 22.03.2025.
+//
+
+import Testing
+@testable import Swipy
+
+struct SwipyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
